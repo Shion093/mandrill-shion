686856320909dc0c226c7901e91932a05de3b20e
Alex Varela <alex.varela@dnamicworld.com>
2017-01-05 22:24:35 -0600

    mandrill testing

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,57 @@
-"use strict";
+'use strict';
 
-module.exports = function (_ref) {
-  var _ref$percent = _ref.percent,
-      percent = _ref$percent === undefined ? 100 : _ref$percent,
-      amount = _ref.amount;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.initialMailer = initialMailer;
+exports.sendSingle = sendSingle;
+exports.createMailObjMultiple = createMailObjMultiple;
 
-  var percentOff = percent / 100 * amount;
-  return percentOff;
-};
+var _mandrill = require('mandrill-api/mandrill');
+
+var _single = require('./single');
+
+var mandrillClient = void 0;
+
+function initialMailer(apiKey) {
+  console.log(apiKey);
+  mandrillClient = new _mandrill.Mandrill(apiKey);
+}
+
+function sendSingle(hola) {
+  console.log(hola);
+  var mailObj = (0, _single.createMailObj)('dnamic-catalog', 'test from npm', 'shion093@gmail.com', 'Alex', true, 'handlebars');
+  sendEmail(mailObj).then(function (result) {
+    console.log(result);
+    return result;
+  });
+}
+
+function createMailObjMultiple(tmpName, subject, userInfo, mergeVars, important, lang) {
+  return {
+    template_name: tmpName,
+    template_content: [],
+    message: {
+      subject: subject,
+      from_email: 'no-reply@dnamicworld.com',
+      from_name: 'dnamicworld',
+      to: userInfo,
+      important: important,
+      merge: true,
+      merge_language: lang,
+      merge_vars: mergeVars
+    },
+    async: false
+  };
+}
+
+function sendEmail(mailObj) {
+  return new Promise(function (resolve, reject) {
+    mandrillClient.messages.sendTemplate(mailObj, function () {
+      resolve({ sent: true });
+    }, function (err) {
+      console.log(err);
+      reject({ sent: false });
+    });
+  });
+}
